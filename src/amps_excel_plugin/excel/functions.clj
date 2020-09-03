994c1deec270f91bd6347957bab1e24da9147800
Abel Quiros <beluchin@gmail.com>
2020-09-03 12:03:18 +0100

    removed logging

--- src/amps_excel_plugin/excel/functions.clj
+++ src/amps_excel_plugin/excel/functions.clj
@@ -5,17 +5,7 @@
    :methods [^:static [^{com.exceljava.jinx.ExcelFunction {}}
                        subscribe [String String] com.exceljava.jinx.Rtd]
              ^:static [^{com.exceljava.jinx.ExcelFunction {:autoResize true}}
-                       expand [java.lang.Object] "[[Ljava.lang.Object;"]
-
-
-             ^:static [^{com.exceljava.jinx.ExcelFunction {}}
-                       oneInt [] int]
-             ^:static [^{com.exceljava.jinx.ExcelFunction {:autoResize true}}
-                       twoInts [] "[[Ljava.lang.Object;"]
-             ^:static [^{com.exceljava.jinx.ExcelFunction {}}
-                       twoIntVectorRtd [] com.exceljava.jinx.Rtd]
-             ^:static [^{com.exceljava.jinx.ExcelFunction {:autoResize true}}
-                       expandVec [java.lang.Object] "[[Ljava.lang.Object;"]])
+                       expand [java.lang.Object] "[[Ljava.lang.Object;"]])
   (:require [amps-excel-plugin.amps :as amps]
             [amps-excel-plugin.core :as core]
             [amps-excel-plugin.shell :as shell]
@@ -36,9 +26,7 @@
                   (invoke [_ msg]
                     (let [json (.getData msg)
                           rows (core/rows (json/parse-string json))]
-                      (logging/info logger json)
-                      (.notify rtd rows)
-                      (logging/info logger rows))))]
+                      (.notify rtd rows))))]
     (doto client
       (.connect uri)
       (.logon)
@@ -47,42 +35,10 @@
 
 (defn java-expand
   [x]
-  (logging/info logger x)
   (if (= clojure.lang.LazySeq (type x))
     (to-array-2d x)
     (to-array-2d [["pending"]])))
 
-
-
-
-(declare two-random-ints-thread two-random-ints)
-
-(defn java-oneInt [] 42)
-
-(defn java-twoInts [] (to-array-2d [[42 43]]))
-
-(defn java-twoIntVectorRtd []
-  (let [rtd (com.exceljava.jinx.Rtd.)
-        thread (two-random-ints-thread rtd)]
-    (.notify rtd [[42 42]])
-    (.start thread)
-    rtd))
-
-(defn- two-random-ints-thread
-  [rtd]
-  (shell/new-rtd-thread
-    (shell/until-interrupted-subr
-      (shell/execute-at-rate-or-ignore-subr
-        (fn [] (let [random-ints [[(rand-int 30) (rand-int 42)]]]
-                 (logging/info logger (with-out-str (pprint/pprint random-ints)))
-                 (.notify rtd random-ints)))
-        0.2))))
-
-(defn java-expandVec
-  [v]
-  (if (= clojure.lang.PersistentVector (type v))
-    (to-array-2d v)
-    (to-array-2d [["pending"]])))
 
 (comment
   (println [1 2])
