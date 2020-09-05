49b600e7c27dcbb6864f05f1c876b8b9b4299ffb
Abel Quiros <beluchin@gmail.com>
2020-09-05 23:49:03 +0100

    reusing clients - get it to work

--- src/amps_excel_plugin/amps.clj
+++ src/amps_excel_plugin/amps.clj
@@ -1,7 +1,15 @@
 (ns amps-excel-plugin.amps
   (:import [com.crankuptheamps.client Client Command MessageHandler]))
 
-(declare get-new-client-name)
+(declare get-new-client get-new-client-name uri->client)
+
+(defn get-client
+  "returns a existing client if possible. Otherwise creates a new client"
+  [uri]
+  (let [u->c (swap! uri->client #(if-not (% uri)
+                                   (assoc % uri (get-new-client uri))
+                                   %))]
+    (u->c uri)))
 
 (defn get-new-client
   [uri]
@@ -14,8 +22,10 @@
 (defn subscribe-json
   "assumes the uri is truly json i.e tcp://.../amps/json/..."
   [uri topic json-consumer]
-  (let [client (get-new-client uri)
+  (let [client (get-client uri)
         command  (.. (Command. "subscribe") (setTopic topic))
         handler  (reify MessageHandler
                    (invoke [_ msg] (json-consumer (.getData msg))))]
     (.executeAsync client command handler)))
+
+(def uri->client (atom {}))
