f22a4007ef735e5d149cc48d122ad18eaa38f3e7
Abel Quiros <beluchin@gmail.com>
2020-12-27 19:56:00 +0000

    value-in

--- src/amps_excel_plugin/functional.clj
+++ src/amps_excel_plugin/functional.clj
@@ -2,7 +2,7 @@
 
 (defn leafpaths
   ;; https://stackoverflow.com/a/21769786/614800
-  "a leafpath is a sequence of keys from the top a value that is not a map"
+  "a leafpath is a sequence of keys from the top to a value that is not a map"
   [m]
   (if (map? m)
     (vec 
@@ -14,3 +14,13 @@
                    [[k]])))
              m))
     []))
+
+(defn value-in
+  "on m, the ks1 lead to a sequence of maps. An map from the sequence
+  is selected using the index-fn and from there the ks2 are used
+  to get to a value. The index-fn takes a sequence and returns an index"
+  [m ks1 index-fn ks2]
+  (-> m
+      (get-in ks1)
+      (#(nth % (index-fn %)))
+      (get-in ks2)))
--- test/amps_excel_plugin/functional_test.clj
+++ test/amps_excel_plugin/functional_test.clj
@@ -8,3 +8,7 @@
     (t/is (= {"a" [{"b" 2} {"c" 3}]}
              (cheshire/parse-string "{\"a\": [{\"b\": 2},{\"c\": 3}]}")))))
 
+(t/deftest value-in-test
+  (t/are [m ks1 index-fn ks2 result] (= result (sut/value-in m ks1 index-fn ks2))
+    {:a [{:b 1}]} [:a] (constantly 0) [:b] 1))
+
