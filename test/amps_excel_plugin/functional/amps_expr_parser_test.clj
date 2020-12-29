8cbc995696c92b3d58efccfd5defef8d995e3668
Abel Quiros <beluchin@gmail.com>
2020-12-29 13:40:27 +0000

    parse subtree selector - expression does not reference a sequential value - red

--- test/amps_excel_plugin/functional/amps_expr_parser_test.clj
+++ test/amps_excel_plugin/functional/amps_expr_parser_test.clj
@@ -4,13 +4,15 @@
 
 (t/deftest parse-subtree-selector-test
   (t/testing "happy path"
-    (let [[ks index-fn] (sut/parse-subtree-selector {"a" [:whatever]}
-                                                    [["a" "b"] 1])]
-      (t/is (and (= ["a"] ks) (= 0 (index-fn [{"b" 1}]))))))
-  (t/testing "supports single and double quotes for strings"
-    (throw (UnsupportedOperationException.)))
-  (t/testing "reliability - no sequential value along the way"
-    (throw (UnsupportedOperationException.)))
+    (let [[ks index-fn] (sut/parse-subtree-selector {:a [:whatever]}
+                                                    [[:a :b] 1])]
+      (t/is (and (= [:a] ks) (= 0 (index-fn [{:b 1}]))))))
+
+  (t/testing "reliability - expression does not reference a sequential value"
+    (t/is (thrown-with-msg? IllegalArgumentException
+                            #"expression does not reference a sequential value"
+                            (sut/parse-subtree-selector {} [:a]))))
+
   (t/testing "reliability - more than one sequential value"
     (throw (UnsupportedOperationException.))))
 
