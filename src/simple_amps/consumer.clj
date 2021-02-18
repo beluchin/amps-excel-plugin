782fe8ef08b758ec6c0db024499b30fa94417345
Abel Quiros <beluchin@gmail.com>
2021-02-18 06:43:20 -0500

    activating/activated notifications to the qvns

--- src/simple_amps/consumer.clj
+++ src/simple_amps/consumer.clj
@@ -2,9 +2,7 @@
 
 (defprotocol QueryValueAndSubscribeConsumer
   (on-value [this x])
-  (on-active-no-sow [this])
-
-  ;; reason: cannot connect | connection was closed
-  (on-inactive [this reason])
-
+  (on-activating [this])
+  (on-activated [this])
+  (on-inactive [this reason]) ;; reason: cannot connect | connection was closed
   (on-unaliased [this]))
