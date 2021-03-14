71ba4e25533e8fdfd45a5d91caa6867743ba52cc
Abel Quiros <beluchin@gmail.com>
2021-03-14 18:46:08 -0400

    removed {src,test}.old

--- src.old/amps_excel_plugin/amps/functional.clj
+++
@@ -1,6 +0,0 @@
-(ns amps-excel-plugin.amps.functional)
-
-(defn components
-  [uri]
-  (zipmap [:host-port :message-type]
-          (rest (re-find #"tcp://([^/]+)/amps/([^/]+)" uri))))
--- test.old/amps_excel_plugin/amps/functional_test.clj
+++
@@ -1,8 +0,0 @@
-(ns amps-excel-plugin.amps.functional-test
-  (:require [amps-excel-plugin.amps.functional :as sut]
-            [clojure.test :as t]))
-
-(t/deftest components-test
-  (t/is (= {:host-port "localhost:8080"
-            :message-type "json"}
-           (sut/components "tcp://localhost:8080/amps/json"))))
