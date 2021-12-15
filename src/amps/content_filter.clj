2eee26f8024f07311d1c8d0eab8e37235b6226f5
Abel Quiros <beluchin@gmail.com>
2021-12-15 11:09:11 +0000

    amps.content-filter

--- src/amps/content_filter.clj
+++ src/amps/content_filter.clj
@@ -1,2 +1,22 @@
-(ns amps.content-filter)
+(ns amps.content-filter
+  (:require andor)
+  (:import [andor Or]))
+
+(defprotocol Remove
+  (remove [cf to-remove]))
+(extend-protocol Remove
+  Object
+  (remove [this to-remove]
+    (when-not (= this to-remove)
+      (throw (UnsupportedOperationException.)))))
+
+(extend-type Or
+  Remove
+  (remove [this to-remove]
+    (let [operand-set (:operand-set this)]
+      (if (contains? operand-set to-remove)
+        (apply andor/or (clojure.core/remove #{to-remove} operand-set))
+        this))))
+
+(def add andor/or)
 
--- test/amps/content_filter_test.clj
+++ test/amps/content_filter_test.clj
@@ -1,4 +1,16 @@
 (ns amps.content-filter-test
-  (:require [amps.content-filter :as sut]
+  (:require andor
+            [amps.content-filter :as sut]
             [clojure.test :as t]))
+
+(t/deftest remove-test
+  (t/testing "remove if added (or'ed) previously"
+    (t/is (= :a (sut/remove (sut/add :a :b) :b)))
+    (t/is (= (andor/or :a :b) (sut/remove (andor/add :a :b) :c))))
+  
+  (t/testing "remove if identical"
+    (t/is (nil? (sut/remove :a :a))))
+  
+  (t/testing "otherwise is unsupported"
+    (t/is (thrown? UnsupportedOperationException (sut/remove :a :b)))))
 
