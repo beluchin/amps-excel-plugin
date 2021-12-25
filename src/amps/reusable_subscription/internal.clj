b755a88b9af87f99e519961fec4c935a84d451f6
Abel Quiros <beluchin@gmail.com>
2021-12-25 19:46:00 +0000

    records for actions to execute at the operational level

--- src/amps/reusable_subscription/internal.clj
+++ src/amps/reusable_subscription/internal.clj
@@ -6,89 +6,44 @@
 ;; Ensures a single reusable subscription to a client/topic. 
 ;;
 ;; It keeps track of all the filters associated with a client/topic to
-;; be used when replacing a filter on the single subscription to the
-;; client/topic. The client needs only to add/remove filters.
+;; be used when replacing a filter on the single subscription. The
+;; client needs only to add/remove filters.
 ;;
 
-(defrecord ^:private Subscribed [filter sub-id command-id])
+(defrecord Subscribe [])
 
-(defn- check-for-ensure [state client topic]
-  state)
+(defrecord ReplaceFilter [filter sub-id])
 
-(defn- check-for-failed-to-subscribe [state client topic]
-  state)
+(defrecord Unsubscribe [command-id])
 
-(defn- check-for-removal [state client topic filter]
-  state)
-
-(defn- check-for-subscribed [state client topic filter sub-id]
-  state)
-
-(declare ->PendingFilterReplacement)
-(defn- pending-filter-replacement [state new-filter]
-  (->PendingFilterReplacement new-filter
-                              (:sub-id state)
-                              (:command-id state)))
-
-(declare ->PendingSubscription)
-(defn- pending-subscription [filter]
-  (->PendingSubscription filter))
-
-(declare ->PendingUnsubscribe)
-(defn- pending-unsubscribe [state]
-  (->PendingUnsubscribe (:command-id state)))
-
-(defrecord PendingSubscription [filter])
-
-(defrecord PendingFilterReplacement [filter sub-id command-id])
-
-(defrecord PendingUnsubscribe [command-id])
-
-(defn ensure
-  "the library keeps track of the filter (andor/optimize'd) to use in
-  replacements.  Client needs only to supply the new filter they want
-  to subscribe to. Returns a new state of the reusable subscriptions"
-  [resubs client topic filter]
+(defn ensure [resubs client topic filter]
   (let [k [client topic]]
-    (if-let [state (get resubs k)]
-      (-> (check-for-ensure state client topic)
-          (pending-filter-replacement (cf/add (:filter state) filter))
-          (#(assoc resubs k %)))
-      (assoc resubs k (pending-subscription filter)))))
+    (if-let [v (get resubs k)]
+      (let [new-filter (cf/add (:filter v) filter)
+            sub-id (:sub-id v)]
+        [(assoc resubs k {:filter new-filter, :sub-id sub-id})
+         (->ReplaceFilter new-filter sub-id)])
+      [(assoc resubs k {:filter filter}) (->Subscribe)])))
 
-(defn remove
-  "returns a new state of the reusable subscriptions"
-  [resubs client topic filter]
+(defn remove [resubs client topic filter]
   (let [k [client topic]
-        state (get resubs k)]
-    (check-for-removal state client topic filter)
-    (if (= (:filter state) filter)
-      (assoc resubs k (pending-unsubscribe state))
-      (assoc resubs k (pending-filter-replacement
-                        state
-                        (cf/remove (:filter state) filter))))))
+        v (get resubs k)]
+    (if (= (:filter v) filter)
+      [(dissoc resubs k) (->Unsubscribe (:command-id v))]
+      (let [new-filter (cf/remove (:filter v) filter)]
+        [(assoc-in resubs [k :filter] new-filter)
+         (->ReplaceFilter new-filter (:sub-id v))]))))
 
-(defn whats-pending
-  "returns one of the pending states or nil if nothing is pending"
-  [resubs client topic]
-  (get resubs [client topic]))
+(defn action [ensure-or-remove-return]
+  (second ensure-or-remove-return))
 
-(defn subscribed
-  "returns a new state of the reusable subscriptions. Throws if a
-  subscription or a filter replacement was not pending for the given filter"
-  [resubs client topic filter sub-id command-id]
-  (check-for-subscribed (get resubs [client topic]) client topic filter sub-id)
-  (assoc resubs [client topic] (->Subscribed filter sub-id command-id)))
+(defn resubs [ensure-or-remove-return]
+  (first ensure-or-remove-return))
 
-(defn failed-to-subscribe
-  "returns a new state of the reusable subscriptions. Throws if a
-  subscription was not pending for the given filter. Calling it while
-  pending a filter replacement is illegal since the sensible thing to do
-  when failing to replace the filter is to unsubscribe"
-  [resubs client topic]
-  (check-for-failed-to-subscribe (get resubs [client topic]) client topic)
+(defn subscribed [resubs client topic sub-id command-id]
+  (update resubs [client topic] merge {:sub-id sub-id, :command-id command-id}))
+
+(defn failed-to-subscribe [resubs client topic]
   (dissoc resubs [client topic]))
 
-(defn disconnected
-  "returns a new state of the reusable subscriptions"
-  [resubs client])
+(defn disconnected [resubs client])
--- test/amps/reusable_subscription/internal_test.clj
+++ test/amps/reusable_subscription/internal_test.clj
@@ -3,8 +3,18 @@
   (:require [amps.reusable-subscription.internal :as sut]
             [clojure.test :as t]))
 
-(defn- ensure [resubs new-filter]
-  (sut/ensure resubs :client :topic new-filter))
+(defn- ensure
+  ([resubs]
+   (ensure resubs :filter))
+  ([resubs filter]
+   (sut/ensure resubs :client :topic filter)))
+
+(declare fresh-ensure subscribed)
+(defn- ensure-filter-replacement []
+  (-> (fresh-ensure)
+      sut/resubs
+      subscribed
+      (ensure :new-filter)))
 
 (defn- failed-to-subscribe [resubs]
   (sut/failed-to-subscribe resubs :client :topic))
@@ -12,17 +22,8 @@
 (defn- fresh-ensure []
   (sut/ensure nil :client :topic :filter))
 
-(defn- pending-filter-replacement
-  ([filter]
-   (pending-filter-replacement filter :command-id))
-  ([filter command-id]
-   (sut/->PendingFilterReplacement filter :sub-id command-id)))
-
-(defn- pending-subscription []
-  (sut/->PendingSubscription :filter))
-
-(defn- pending-unsubscribe []
-  (sut/->PendingUnsubscribe :command-id))
+(defn- replace-filter [filter]
+  (sut/->ReplaceFilter filter :sub-id))
 
 (defn- remove
   ([resubs]
@@ -30,48 +31,61 @@
   ([resubs filter]
    (sut/remove resubs :client :topic filter)))
 
+(declare subscribed)
+(defn- resubs-after-fresh-subscribe []
+  (-> (fresh-ensure)
+      sut/resubs
+      subscribed))
+
+(defn- subscribe []
+  (sut/->Subscribe))
+
 (defn- subscribed
   ([resubs]
-   (subscribed resubs :filter :command-id))
-  ([resubs filter]
-   (subscribed resubs filter :command-id))
-  ([resubs filter command-id]
-   (sut/subscribed resubs :client :topic filter :sub-id command-id)))
+   (subscribed resubs :command-id))
+  ([resubs command-id]
+   (sut/subscribed resubs :client :topic :sub-id command-id)))
 
-(defn- whats-pending [resubs]
-  (sut/whats-pending resubs :client :topic))
+(defn- unsubscribe []
+  (sut/->Unsubscribe :command-id))
 
 (t/deftest ensure-test
   (t/testing "a new subscription"
-    (t/is (= (pending-subscription)
-             (whats-pending (fresh-ensure)))))
+    (t/is (= (subscribe) (sut/action (fresh-ensure)))))
 
   (t/testing "updating the filter"
-    (t/testing "adding a new filter"
-      (t/is (= (pending-filter-replacement (andor/or :new-filter :filter))
-               (-> (fresh-ensure)
-                   (subscribed)
-                   (ensure :new-filter)
-                   (whats-pending)))))))
+    (t/is (= (replace-filter (andor/or :new-filter :filter))
+             (-> (resubs-after-fresh-subscribe)
+                 (ensure :new-filter)
+                 sut/action)))))
 
-(t/deftest failed-to-subscribe-test 
-  (t/is (nil? (-> (fresh-ensure)
-                  (failed-to-subscribe)
-                  (whats-pending)))) )
+(t/deftest failed-to-subscribe-test
+  (t/testing "after the first subscription"
+    (t/is (= (subscribe) (-> (fresh-ensure)
+                             sut/resubs
+                             failed-to-subscribe
+                             ensure
+                             sut/action))))
+
+  (t/testing "after having replaced the filter"
+    (t/is (= (subscribe) (-> (ensure-filter-replacement)
+                             sut/resubs
+                             failed-to-subscribe
+                             ensure
+                             sut/action)))))
 
 (t/deftest remove-test
   (t/testing "the only filter"
-    (t/is (= (pending-unsubscribe)
-             (-> (fresh-ensure)
-                 (subscribed)
-                 (remove)
-                 (whats-pending)))))
+    (t/is (= (unsubscribe)
+             (-> (resubs-after-fresh-subscribe)
+                 remove
+                 sut/action))))
 
   (t/testing "one of many"
-    (t/is (= (pending-filter-replacement :filter :command-id-2)
-             (-> (fresh-ensure)
-                 (subscribed :filter :command-id-1)
+    (t/is (= (replace-filter :new-filter)
+             (-> (resubs-after-fresh-subscribe)
                  (ensure :new-filter)
-                 (subscribed (andor/or :filter :new-filter) :command-id-2)
-                 (remove :new-filter)
-                 (whats-pending))))))
+                 sut/resubs
+                 subscribed
+                 (remove :filter)
+                 sut/action)))))
