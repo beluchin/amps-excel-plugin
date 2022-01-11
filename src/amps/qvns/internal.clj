8217324c9d6ef9a2345377376cf2a9614ec30a44
Abel Quiros <beluchin@gmail.com>
2022-01-11 10:28:14 +0000

    remove - disconnect / unsubscribe (different topic) - skeletons

--- src/amps/qvns/internal.clj
+++ src/amps/qvns/internal.clj
@@ -1,6 +1,7 @@
 (ns amps.qvns.internal
   (:refer-clojure :exclude [ensure]))
 
+(defrecord Disconnect [client])
 (defrecord InitialSubscription [topic+content-filter--coll
                                 activating-runnable-coll])
 (defrecord ReplaceFilter [content-filter sub-id command-id])
@@ -10,5 +11,7 @@
 (defn ensure [mgr qvns])
 
 (defn mgr [result])
+
+(defn remove [mgr qvns])
 
 (defn subscribed [mgr uri topic content-filter sub-id command-id])
--- test/amps/qvns/internal_test.clj
+++ test/amps/qvns/internal_test.clj
@@ -1,14 +1,21 @@
 (ns amps.qvns.internal-test
-  (:refer-clojure :exclude [ensure])
+  (:refer-clojure :exclude [ensure remove])
   (:require [amps.qvns.internal :as sut]
             [clojure.test :as t]
             test-helpers))
 
 (def ^:private action sut/action)
 
+(defn- disconnect []
+  (sut/->Disconnect :client))
+
 (defn- ensure
   ([mgr] (ensure mgr :qvns))
   ([mgr qvns] (sut/ensure mgr qvns)))
+
+(declare mgr subscribed)
+(defn- ensured-subscription-mgr []
+  (-> nil ensure mgr subscribed mgr))
 
 (defn- initial-subscription []
   (sut/->InitialSubscription [[:topic :content-filter]] [:activating-runnable]))
@@ -24,11 +31,18 @@
                                        event-handlers)
          overrides))
 
+(defn- remove
+  ([mgr] (remove mgr :qvns))
+  ([mgr qvns] (sut/remove mgr qvns)))
+
 (defn- replace-filter []
   (sut/->ReplaceFilter :content-filter :sub-id :command-id))
 
 (defn- subscribed [mgr]
-  (sut/subscribed mgr :uri :topic :content-filter :sub-id :command-id))
+  (sut/subscribed mgr :client :topic :content-filter :sub-id :command-id))
+
+(defn- unsubscribe [mgr]
+  (throw (UnsupportedOperationException.)))
 
 (t/deftest ensure-test
   (t/testing "initial subscription"
@@ -41,6 +55,25 @@
                                   ensure
                                   mgr
                                   subscribed
+                                  mgr
                                   (ensure (qvns :item-filter :new-item-filter))
                                   action)))))
+
+
+(t/deftest remove-test 
+  (t/testing "disconnect"
+    (t/is (= (disconnect)
+             (-> (ensured-subscription-mgr)
+                 remove
+                 action))))
+
+  (t/testing "unsubscribe - different topic" 
+    (t/is (= (unsubscribe)
+             (-> (ensured-subscription-mgr)
+                 (ensure (qvns :client :client :topic :topic-y))
+                 mgr
+                 subscribed
+                 mgr
+                 (remove (qvns :client :client :topic :topic-y))
+                 action)))))
 
